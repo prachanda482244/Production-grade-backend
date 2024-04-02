55737e9370679d0db84dd040d551fedfe09107a1
Prachana <pralhadrana123@gmail.com>
2024-04-02 22:00:54 +0545

    create the app

--- src/index.js
+++ src/index.js
@@ -1,4 +1,16 @@
-import express from "express";
 import connectToDb from "./db/connect.js";
+import app from "./app.js";
 
 connectToDb()
+.then(()=>{
+      app.on("error",(err)=>{
+            console.error(err)
+            throw err
+      })
+      app.listen(process.env.PORT || 8000,()=>{
+            console.log('Server running');
+      })
+})
+.catch((err)=>{
+console.error("Error:",err)
+})
