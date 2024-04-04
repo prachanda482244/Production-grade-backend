c9165c66fb8e356740072187aae3b305e924a79d
Prachana <pralhadrana123@gmail.com>
2024-04-04 19:44:26 +0545

    upload files on cloudinary

--- src/constants.js
+++ src/constants.js
@@ -1,3 +1,6 @@
 import { config } from "dotenv"
 config()
 export const DB_NAME = "videotube"
+export const CLOUDINARY_CLOUD_NAME = process.env.CLOUDINARY_CLOUD_NAME
+export const CLOUDINARY_API_KEY = process.env.CLOUDINARY_API_KEY
+export const CLOUDINARY_API_SECRET = process.env.CLOUDINARY_API_SECRET
---
+++ src/utils/cloudinary.js
@@ -0,0 +1,31 @@
+import { v2 as cloudinary } from "cloudinary"
+import fs from "fs"
+import {
+  CLOUDINARY_API_KEY,
+  CLOUDINARY_API_SECRET,
+  CLOUDINARY_CLOUD_NAME,
+} from "../constants.js"
+
+cloudinary.config({
+  cloud_name: CLOUDINARY_CLOUD_NAME,
+  api_key: CLOUDINARY_API_KEY,
+  api_secret: CLOUDINARY_API_SECRET,
+})
+
+const uploadOnCloudinary = async (localFilePath) => {
+  try {
+    if (!localFilePath) return null
+    //upload the file on cloudinary
+    const response = await cloudinary.uploader.upload(localFilePath, {
+      resource_type: "auto",
+    })
+    //file has been uploaded successfully
+    console.log("File is uploaded on cloudinary \n", response.url)
+    return response
+  } catch (error) {
+    fs.unlinkSync(localFilePath) // remove the locally saved temporary file as the upload operation got failed
+    return null
+  }
+}
+
+export { uploadOnCloudinary }
