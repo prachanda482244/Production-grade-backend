e2452d98381a6b699265908cbe7f63021d1879f5
Prachana <pralhadrana123@gmail.com>
2024-04-10 18:02:11 +0545

    Add comment like tweet playlist model

---
+++ src/models/like.model.js
@@ -0,0 +1,24 @@
+import { Schema, model } from "mongoose"
+
+const likeSchema = new Schema(
+  {
+    vide: {
+      type: Schema.Types.ObjectId,
+      ref: "Video",
+    },
+    comment: {
+      type: Schema.Types.ObjectId,
+      ref: "Comment",
+    },
+    tweet: {
+      type: Schema.Types.ObjectId,
+      ref: "Tweet",
+    },
+    likedBy: {
+      type: Schema.Types.ObjectId,
+      ref: "User",
+    },
+  },
+  { timestamps: true }
+)
+export const Like = model("Like", likeSchema)
